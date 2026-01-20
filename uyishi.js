44e3929521b1fc3a85e3e5762256b6065ae063c9
tolqinboyevsaidjon <tolqinboyevsaidjon925@gmail.com>
2026-01-20 15:05:45 +0500

    java script uuy ishi
    bu yerda eng kata va juft toq sonlarni topivchii web sahifa qilingan

---
+++ uyishi.js
@@ -0,0 +1,22 @@
+function juftVatoqson(son){
+  if(son % 2 == 0){
+    console.log("Juft son");
+  } else {
+    console.log("Toq son");
+  }
+}
+juftVatoqson(2);
+juftVatoqson(5);
+
+function EngKattasi(a,b,c) {
+let maksimum = a;
+ if (b > maksimum) {
+  maksimum = b;
+ }
+if (c > maksimum) {
+  maksimum = c;
+}
+console.log ("Eng kotta son",maksimum)
+}
+EngKattasi(3,7,9);
+EngKattasi(8,3,5);
