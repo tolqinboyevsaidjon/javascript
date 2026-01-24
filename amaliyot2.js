8893dd0cb48e16f4eaec412a3e1f272db8b7c609
tolqinboyevsaidjon <tolqinboyevsaidjon925@gmail.com>
2026-01-24 16:59:04 +0500

    amaliyot2
    ---

---
+++ amaliyot2.js
@@ -0,0 +1,29 @@
+const car = {
+  nomi: "Chevrolet",
+  rangi: "oq",
+  yili: 2022
+};
+
+console.log("Mashina:", car);
+
+let foydalanuvchi = {
+  ism: "Jasur",
+  yosh: 25,
+  shahar: "Toshkent"
+};
+
+foydalanuvchi.email = "jasur@gmail.com";
+foydalanuvchi.yosh = 26;
+
+console.log("Foydalanuvchi:", foydalanuvchi);
+
+let talaba4 = {
+  ism: "Madina",
+  fakultet: "Informatika",
+  shahar: "Samarqand",
+  yil: 2
+};
+
+delete talaba4.shahar;
+
+console.log("Talaba:", talaba4);
