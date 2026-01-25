b80635121e02a444c1e5b4f819007fe1081974a2
tolqinboyevsaidjon <tolqinboyevsaidjon925@gmail.com>
2026-01-25 11:13:55 +0500

    yakshanba amaliyoti
    ---

---
+++ yakshanbaamaliyot.js
@@ -0,0 +1,25 @@
+//1
+// let ball = +prompt("Ball:")
+// ball=50
+
+// console.log(
+//   ball > 90 ? "A'lo" :
+//   ball >= 70 ? "Yaxshi" :
+//   ball >= 50 ? "Qoniqarli" :
+//   "Qoniqarsiz"
+// )
+
+//2
+let harorat = 30
+
+if ( harorat > 30 ){
+    console.log("Juda issiq")
+} else if ( harorat <= 30 ){
+    console.log("illiq")
+} else if ( harorat <= 20 ){
+    console.log("sovuq")
+} else {
+    console.log("juda sovuq")
+}
+
+
