0e72640454bd4e5f86a7ed15b4fd5e402b3ea9cc
tolqinboyevsaidjon <tolqinboyevsaidjon925@gmail.com>
2026-01-30 20:53:41 +0500

    uy ishi 4
    ---

---
+++ uyishi4.js
@@ -0,0 +1,20 @@
+//1
+const mahsulot = "laptop";
+const narxi = 8500000;
+const rangi = "kumush";
+
+const xabar = ` siz ${rangi} rangli ${mahsulot} tanladingiz . narxi: ${narxi} som`
+
+console . log (xabar)
+
+//2
+const mahsulot2 = "telefon";
+const price = 5000000;
+const chegirma = 15;
+
+const chegirmasumasi = price * 15 / 100
+const yanginarx = price - chegirmasumasi
+
+const text = `${mahsulot2}-asl narxi: ${price}, chegirma: ${chegirma}%, yangi narxi: ${yanginarx} som`;
+
+console.log (text)
